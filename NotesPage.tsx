1dad7fcea088c8e1cc9b98da178ff32c1ac59339
Mahirul Alam Chowdhury <62341446+Slyyoursenpai@users.noreply.github.com>
2026-06-05 16:14:40 +0900

    added local storage, search
    Notes can now be locally stored, deleted, and searched

---
+++ NotesPage.tsx
@@ -0,0 +1,74 @@
+import { useState } from "react"
+import NoteCard from "../components/NoteCard"
+
+type Note = {
+  title: string
+  content: string
+}
+
+export default function NotesPage({
+  notes,
+  setNotes,
+  deleteNote
+}: {
+  notes: Note[]
+  setNotes: React.Dispatch<React.SetStateAction<Note[]>>
+  deleteNote: (indexToDelete: number) => void
+}) {
+  const [title, setTitle] = useState("")
+  const [content, setContent] = useState("")
+  const [search, setSearch] = useState("")
+
+  const addNote = () => {
+    if (!title || !content) return
+
+    setNotes([
+      ...notes,
+      {
+        title: title,
+        content: content
+      }
+    ])
+
+    setTitle("")
+    setContent("")
+  }
+
+  return (
+    <div>
+      <h1>Notes Page</h1>
+
+      <input
+        placeholder="Search Notes"
+        value={search}
+        onChange={(e)=>setSearch(e.target.value)}
+      />
+      
+      <input
+        placeholder="Title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+
+      <input
+        placeholder="Content"
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+      />
+
+      <button onClick={addNote}>Add Note</button>
+
+      {notes.filter((note) =>
+          note.title.toLowerCase().includes(search.toLowerCase()) || 
+          note.content.toLowerCase().includes(search.toLowerCase())
+        ).map((note, index) => ( 
+        <NoteCard
+          key={index}
+          title={note.title}
+          content={note.content}
+          onDelete={()=>deleteNote(index)}
+        />
+      ))}
+    </div>
+  )
+}
