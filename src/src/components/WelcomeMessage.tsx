b9bddff792a351a00adda6daeea18380c32c0e77
Mahirul Alam Chowdhury <62341446+Slyyoursenpai@users.noreply.github.com>
2026-06-15 16:22:13 +0600

    Delete src directory

--- src/src/components/WelcomeMessage.tsx
+++
@@ -1,5 +0,0 @@
-function WelcomeMessage(props:{name:string}) {
-  return <h2>Welcome {props.name} to NotePal</h2>
-}
-
-export default WelcomeMessage
--- src/src/types/Note.ts
+++
@@ -1,6 +0,0 @@
-export type Note = {
-  id: number
-  title: string
-  content: string
-  createdAt: string
-}
